f028a9164f4937429e6cc96e00dd88ec918e3104
Christopher Mitchell <76568996+CMitchell5619@users.noreply.github.com>
2021-04-28 09:57:54 -0600

    init

---
+++ src/pages/CharacterPage.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="character">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          Name
+        </div>
+        <div class="col-3">
+          Heritage
+        </div>
+        <div class="col-3">
+          Occupation
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          Appearance
+        </div>
+        <div class="col-6">
+          Traits
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          Photo Box
+        </div>
+        <div class="col-8">
+          Ability Scores
+          <div class="row"></div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          Gear Box
+        </div>
+        <div class="col-8">
+          <div class="row">
+            <div class="col-2">
+              Armor
+            </div>
+            <div class="col-4">
+              HP
+            </div>
+            <div class="col-4">
+              XP
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              Notes Box
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Character',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    <img src="@/assets/img/Jacq.jpg" alt="Sprigg">
+    <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
+      <span class="mx-2 text-white">Freebooters on the Frontier 2E</span>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped lang="scss">
+.home{
+  text-align: center;
+  user-select: none;
+  > img{
+    height: 200px;
+    width: 200px;
+  }
+}
+</style>
