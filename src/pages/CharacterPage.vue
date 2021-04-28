4190a0e2cc82178ec291ba621550adb307a732cf
Christopher Mitchell <76568996+CMitchell5619@users.noreply.github.com>
2021-04-28 10:33:05 -0600

    Character page layout is done

--- src/pages/CharacterPage.vue
+++ src/pages/CharacterPage.vue
@@ -1,52 +1,157 @@
 <template>
-  <div class="character">
-    <div class="container">
-      <div class="row">
-        <div class="col-6">
+  <div class="container">
+    <div class="row mt-3">
+      <div class="col-6">
+        <div class="card shadow-sm">
           Name
         </div>
-        <div class="col-3">
+      </div>
+      <div class="col-3">
+        <div class="card shadow-sm">
           Heritage
         </div>
-        <div class="col-3">
+      </div>
+      <div class="col-3">
+        <div class="card shadow-sm">
           Occupation
         </div>
       </div>
-      <div class="row">
-        <div class="col-6">
+    </div>
+    <div class="row mt-3">
+      <div class="col-6">
+        <div class="card shadow-sm">
           Appearance
         </div>
-        <div class="col-6">
+      </div>
+      <div class="col-6">
+        <div class="card shadow-sm">
           Traits
         </div>
       </div>
-      <div class="row">
-        <div class="col-4">
+    </div>
+    <div class="row mt-3">
+      <div class="col-4">
+        <div class="card shadow-sm">
           Photo Box
         </div>
-        <div class="col-8">
+      </div>
+      <div class="col-8">
+        <div class="card shadow-sm">
           Ability Scores
-          <div class="row"></div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="card shadow-sm">
+              Strength
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              STR
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="card shadow-sm">
+              Constitution
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              CON
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="card shadow-sm">
+              Dexterity
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              DEX
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="card shadow-sm">
+              Intelligence
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              INT
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="card shadow-sm">
+              Wisdom
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              WIS
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="card shadow-sm">
+              Charisma
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              CHA
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="card shadow-sm">
+              Luck
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              LUC
+            </div>
+          </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-4">
+    </div>
+    <div class="row mt-3">
+      <div class="col-4">
+        <div class="card shadow-sm">
           Gear Box
         </div>
-        <div class="col-8">
-          <div class="row">
-            <div class="col-2">
+      </div>
+      <div class="col-8">
+        <div class="row">
+          <div class="col-2">
+            <div class="card shadow-sm">
               Armor
             </div>
-            <div class="col-4">
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
               HP
             </div>
-            <div class="col-4">
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
               XP
             </div>
           </div>
-          <div class="row">
-            <div class="col">
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="card shadow-sm">
               Notes Box
             </div>
           </div>
