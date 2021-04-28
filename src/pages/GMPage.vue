1b9e42d80164855a1502a9306a7027b3d5621144
Christopher Mitchell <76568996+CMitchell5619@users.noreply.github.com>
2021-04-28 11:21:24 -0600

    Built out structure for front-end

---
+++ src/pages/GMPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="gm-page">
+    This is the GM Page.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GMPage',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
